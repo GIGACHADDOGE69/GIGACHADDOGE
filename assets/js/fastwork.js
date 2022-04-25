25f536c2faf8d894d8ebc03848eaf44ed1794c1d
emeeee17 <103935543+emeeee17@users.noreply.github.com>
2022-04-25 20:58:29 +0700

    Update fastwork.js

--- assets/js/fastwork.js
+++ assets/js/fastwork.js
@@ -80,7 +80,7 @@ async function updatemintinfo() {
 async function updatemintcount() {
     const minted = await window.NFTcontract.methods.totalSupply().call();
     const info = await window.NFTcontract.methods.roundInfo(keymintcontract).call();
-    document.getElementById('mintedcount').innerHTML = `🐱 : ${minted-soldoutnft} / ${info[1]}`;
+    document.getElementById('mintedcount').innerHTML = `🔥 : ${minted-soldoutnft} / ${info[1]}`;
 }
 
 function generateRandomInteger(max) {
